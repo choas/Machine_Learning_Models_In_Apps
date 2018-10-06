ca51a4f0a09124b3c26234022288c15ccf31a317
Lars Gregori <lars.gregori@sap.com>
2018-10-06 02:18:38 +0200

    SwiftLint

--- XOR/iOS/XOR/ViewController.swift
+++ XOR/iOS/XOR/ViewController.swift
@@ -9,35 +9,35 @@
 import UIKit
 import CoreML
 
-
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var out00: UILabel!
     @IBOutlet weak var out01: UILabel!
     @IBOutlet weak var out10: UILabel!
     @IBOutlet weak var out11: UILabel!
-    
+
     let model = xor()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         out00.text = "\(predict(0, 0))"
         out01.text = "\(predict(0, 1))"
         out10.text = "\(predict(1, 0))"
         out11.text = "\(predict(1, 1))"
     }
-    
-    func predict(_ a: Int, _ b: Int) -> NSNumber {
-        let input_data = try! MLMultiArray(shape:[2],
-                                           dataType: MLMultiArrayDataType.float32)
-        input_data[0] = NSNumber(value: a)
-        input_data[1] = NSNumber(value: b)
-        
-        let xor_input = xorInput(input: input_data)
-        let prediction = try! model.prediction(input: xor_input)
-        print(prediction.result.count)
-        return prediction.result[0]
+
+    func predict(_ value1: Int, _ value2: Int) -> NSNumber {
+        guard let inputData = try? MLMultiArray(shape: [2], dataType: MLMultiArrayDataType.float32) else {
+            return -1
+        }
+        inputData[0] = NSNumber(value: value1)
+        inputData[1] = NSNumber(value: value2)
+
+        let input = xorInput(input: inputData)
+        if let prediction = try? model.prediction(input: input) {
+            return prediction.result[0]
+        }
+        return -1
     }
 }
-
